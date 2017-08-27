44addfc0664d74417b917c3de5a39f44284e87bf
hvestamila <31381666+hvestamila@users.noreply.github.com>
2017-08-27 20:28:06 +0300

    Added routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const config = require('./config');
+const Routes = require('./api/routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -12,6 +13,8 @@ app.use(express.static(__dirname + '/web'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+Routes(app);
 
 app.listen(config.server.port, config.server.host, function () {
     console.log(`Server ${config.server.host} is listening on port ${config.server.port}`)
