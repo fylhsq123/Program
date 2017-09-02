44839ebede726d16e886586d526a9ef3959f0704
hvestamila <31381666+hvestamila@users.noreply.github.com>
2017-09-02 11:21:21 +0300

    Moved controllers into separate file

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,10 @@
+const Controllers = require('./controllers');
+const Config = require('../config');
+
+let controllers = new Controllers(Config.db.url);
+
 let Routes = function (app) {
     app.route('/findSimile/:connectingWord')
-        .get(function (req, res) {
-            var num_left = parseInt(req.query.num_left) || 5,
-                num_right = parseInt(req.query.num_right) || 5,
-                zoonym = req.query.zoonym || '';
-            console.log(typeof num_left, num_right)
-            res.status(200).send({reseived: {connectingWord: req.params.connectingWord, num_left, num_right, zoonym}});
-        })
-}
+        .get(controllers.findSimiles);
+};
 module.exports = Routes;
